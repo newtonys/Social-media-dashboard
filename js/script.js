b1245fe2b341445593026666403ba9187670552e
Yosef El-manawy <yusuf.hussien42@gmail.com>
2025-02-21 19:12:41 +0200

    building the toggle button functionailty to switch between light and dark mode in js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,54 @@
-console.log("hello");
+const darkButton = document.getElementById("dark");
+const lightButton = document.getElementById("light");
+
+const setColorMode = () => {
+  if (localStorage.getItem("colorMode") == "dark") {
+    setDarkMode();
+    darkButton.click();
+  } else if (localStorage.getItem("colorMode") == "light") {
+    setLightMode();
+    lightButton.click();
+  }
+};
+
+const checkMode = () => {
+  if (localStorage.getItem("colorMode") == null) {
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+      lightButton.click();
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      darkButton.click();
+    }
+  }
+};
+
+const checkModeChange = () => {
+  window
+    .matchMedia("(prefers-color-scheme: dark)")
+    .addEventListener("change", (event) => {
+      checkMode;
+    });
+};
+
+const setDarkMode = () => {
+  document.querySelector("body").classList = "dark";
+};
+const setLightMode = () => {
+  document.querySelector("body").classList = "light";
+};
+
+setColorMode();
+checkMode();
+checkModeChange();
+const radioButtons = document.querySelectorAll(".toggle__wrapper input");
+
+for (let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].addEventListener("click", (event) => {
+    if (darkButton.checked) {
+      setDarkMode();
+      localStorage.setItem("colorMode", "dark");
+    } else if (lightButton.checked) {
+      localStorage.setItem("colorMode", "light");
+      setLightMode();
+    }
+  });
+}
